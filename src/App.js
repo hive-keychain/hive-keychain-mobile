682d9040fa246ed738939865f38a440894256729
Stoodkev <quentin.correa@gmail.com>
2020-06-10 12:42:18 +0800

    setup navigation accross the App

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import Signup from './screens/Signup';
+import AddAccount from './screens/addAccounts/AddAccount';
+import {setNavigator} from './navigationRef';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer ref={(navigator) => setNavigator(navigator)}>
       <Stack.Navigator>
-        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="SignupScreen" component={Signup} />
+        <Stack.Screen name="AddAccountScreen" component={AddAccount} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,8 @@
 import {SIGN_UP} from './types';
+import {navigate} from '../navigationRef';
 
 export const signUp = (pwd) => {
+  console.log('navigate out');
+  navigate('AddAccountScreen');
   return {type: SIGN_UP, payload: pwd};
 };
