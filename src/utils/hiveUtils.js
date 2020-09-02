f34116195bb2867dcdd3137d5ba43b1d12d60519
Stoodkev <quentin.correa@gmail.com>
2020-09-02 17:46:20 +0800

    add hive conversion functions

---
+++ src/utils/hiveUtils.js
@@ -0,0 +1,95 @@
+import hive, {client} from './dhive';
+
+const HIVE_VOTING_MANA_REGENERATION_SECONDS = 432000;
+const HIVE_100_PERCENT = 10000;
+
+export const getVP = (account) => {
+  if (!account.name) {
+    return null;
+  }
+  const estimated_max = getEffectiveVestingSharesPerAccount(account) * 1000000;
+  const current_mana = parseFloat(account.voting_manabar.current_mana);
+  const last_update_time = account.voting_manabar.last_update_time;
+  const diff_in_seconds = Math.round(Date.now() / 1000 - last_update_time);
+  let estimated_mana =
+    current_mana +
+    (diff_in_seconds * estimated_max) / HIVE_VOTING_MANA_REGENERATION_SECONDS;
+  if (estimated_mana > estimated_max) {
+    estimated_mana = estimated_max;
+  }
+  const estimated_pct = (estimated_mana / estimated_max) * 100;
+  return estimated_pct;
+};
+
+const getEffectiveVestingSharesPerAccount = (account) => {
+  const effective_vesting_shares =
+    parseFloat(account.vesting_shares.replace(' VESTS', '')) +
+    parseFloat(account.received_vesting_shares.replace(' VESTS', '')) -
+    parseFloat(account.delegated_vesting_shares.replace(' VESTS', ''));
+  return effective_vesting_shares;
+};
+
+export const getVotingDollarsPerAccount = (
+  voteWeight,
+  properties,
+  account,
+  full,
+) => {
+  if (!properties.globals || !account.name) {
+    return null;
+  }
+  const vp = getVP(account) * 100;
+  const rewardBalance = getRewardBalance(properties);
+  const recentClaims = getRecentClaims(properties);
+  const hivePrice = getHivePrice(properties);
+  const votePowerReserveRate = getVotePowerReserveRate(properties);
+  console.log(
+    'aaaa',
+    vp,
+    rewardBalance,
+    recentClaims,
+    hivePrice,
+    votePowerReserveRate,
+  );
+  if (rewardBalance && recentClaims && hivePrice && votePowerReserveRate) {
+    const effective_vesting_shares = Math.round(
+      getEffectiveVestingSharesPerAccount(account) * 1000000,
+    );
+    const current_power = full ? 10000 : vp;
+    const weight = voteWeight * 100;
+
+    const max_vote_denom =
+      (votePowerReserveRate * HIVE_VOTING_MANA_REGENERATION_SECONDS) /
+      (60 * 60 * 24);
+    let used_power = Math.round((current_power * weight) / HIVE_100_PERCENT);
+    used_power = Math.round((used_power + max_vote_denom - 1) / max_vote_denom);
+    const rshares = Math.round(
+      (effective_vesting_shares * used_power) / HIVE_100_PERCENT,
+    );
+    const voteValue = ((rshares * rewardBalance) / recentClaims) * hivePrice;
+    return voteValue.toFixed(2);
+  } else {
+    return;
+  }
+};
+export const getRC = async (account) => {
+  const rc = await client.rc.calculateRCMana(account.name);
+  console.log(rc);
+  return 54;
+};
+
+const getRewardBalance = (properties) => {
+  return parseFloat(properties.rewardFund.reward_balance);
+};
+
+const getRecentClaims = (properties) => {
+  return parseInt(properties.rewardFund.recent_claims, 10);
+};
+
+const getHivePrice = (properties) => {
+  return parseFloat(properties.price.base) / parseFloat(properties.price.quote);
+};
+
+const getVotePowerReserveRate = (properties) => {
+  return properties.globals.vote_power_reserve_rate;
+};
