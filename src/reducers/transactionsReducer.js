b4c2675ac80631527f2916496f87e48ae0873431
Stoodkev <quentin.correa@gmail.com>
2020-09-21 16:37:46 +0800

    transactions reducer

---
+++ src/reducers/transactionsReducer.js
@@ -0,0 +1,12 @@
+import {INIT_TRANSACTIONS, ADD_TRANSACTIONS} from '../actions/types';
+
+export default (state = [], {type, payload}) => {
+  switch (type) {
+    case INIT_TRANSACTIONS:
+      return payload;
+    case ADD_TRANSACTIONS:
+      return {...state, ...payload};
+    default:
+      return state;
+  }
+};
