68764c2fd4476ea1b87b8f6591b33d00da4f5e34
Stoodkev <quentin.correa@gmail.com>
2020-10-22 16:16:09 +0800

    reorganize action types

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,9 +1,17 @@
+// keychain
 export const SIGN_UP = 'SIGN_UP';
-export const ADD_ACCOUNT = 'ADD_ACCOUNT';
-export const FORGET_ACCOUNTS = 'FORGET_ACCOUNTS';
 export const UNLOCK = 'UNLOCK';
 export const LOCK = 'LOCK';
 export const INIT_ACCOUNTS = 'INIT_ACCOUNTS';
+
+// accounts
+export const ADD_ACCOUNT = 'ADD_ACCOUNT';
+export const FORGET_ACCOUNTS = 'FORGET_ACCOUNTS';
+export const FORGET_ACCOUNT = 'FORGET_ACCOUNT';
+export const ADD_KEY = 'ADD_KEY';
+export const FORGET_KEY = 'FORGET_KEY';
+
+// hive
 export const ACTIVE_ACCOUNT = 'ACTIVE_ACCOUNT';
 export const GLOBAL_PROPS = 'GLOBAL_PROPS';
 export const ACTIVE_ACCOUNT_RC = 'ACTIVE_ACCOUNT_RC';
@@ -12,6 +20,8 @@ export const INIT_TRANSACTIONS = 'INIT_TRANSACTIONS';
 export const ADD_TRANSACTIONS = 'ADD_TRANSACTIONS';
 export const FETCH_DELEGATEES = 'FETCH_DELEGATEES';
 export const FETCH_DELEGATORS = 'FETCH_DELEGATORS';
+
+// hive engine
 export const LOAD_TOKENS = 'LOAD_TOKENS';
 export const LOAD_USER_TOKENS = 'LOAD_USER_TOKENS';
 export const LOAD_TOKENS_MARKET = 'LOAD_TOKENS_MARKET';
