7f75fd40376f9a4470ecaff5f51a237c488f0f82
steemplus <quentin.correa@gmail.com>
2020-11-20 16:02:48 +0800

    add drawer header with user name and picture

---
+++ src/components/drawer/Header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, StyleSheet, useWindowDimensions} from 'react-native';
+import UserProfilePicture from 'components/ui/UserProfilePicture';
+
+export default ({username}) => {
+  const {width, height} = useWindowDimensions();
+  const styles = getDimensionedStyles({width, height});
+  return (
+    <View style={styles.container}>
+      <UserProfilePicture username={username} style={styles.image} />
+      <Text style={styles.username}>@{username}</Text>
+    </View>
+  );
+};
+
+const getDimensionedStyles = ({width, height}) =>
+  StyleSheet.create({
+    container: {
+      marginTop: 40,
+      paddingBottom: 40,
+      borderBottomColor: 'white',
+      borderBottomWidth: 1,
+      display: 'flex',
+      alignItems: 'center',
+    },
+    username: {color: 'white', fontSize: 20},
+    image: {
+      width: width / 5,
+      height: width / 5,
+      borderRadius: width / 10,
+      marginBottom: 10,
+    },
+  });
