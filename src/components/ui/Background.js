cc933df77bfa24397479333cf53e08467be1971b
steemplus <quentin.correa@gmail.com>
2020-11-25 18:47:03 +0800

    fix background image height

--- src/components/ui/Background.js
+++ src/components/ui/Background.js
@@ -1,21 +1,27 @@
 import React from 'react';
 import {ImageBackground, StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+//import {SafeAreaView} from 'react-native-safe-area-context';
 
 const imageBgd = require('assets/background.png');
 
 export default (props) => {
   return (
-    <SafeAreaView>
-      <ImageBackground
-        {...props}
-        source={imageBgd}
-        style={{...styles.imageBgd, ...props.style}}>
-        {props.children}
-      </ImageBackground>
-    </SafeAreaView>
+    <ImageBackground
+      {...props}
+      source={imageBgd}
+      style={{...styles.imageBgd, ...props.style}}>
+      {props.children}
+    </ImageBackground>
   );
 };
+// <SafeAreaView style={{backgroundColor:'blue'}}>
+//   <ImageBackground
+//     {...props}
+//     source={imageBgd}
+//     style={{...styles.imageBgd, ...props.style}}>
+//     {props.children}
+//   </ImageBackground>
+// </SafeAreaView>
 
 const styles = StyleSheet.create({
   imageBgd: {width: '100%', height: '100%'},
