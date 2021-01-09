f1f1ea4f7efb47c437b8a713fe48739e5736db3c
stoodkev <quentin.correa@gmail.com>
2021-01-09 22:39:02 +0100

    add conversion list

--- src/components/operations/Convert.js
+++ src/components/operations/Convert.js
@@ -1,5 +1,12 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, Keyboard} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  Keyboard,
+  TouchableOpacity,
+  FlatList,
+  View,
+} from 'react-native';
 import {connect} from 'react-redux';
 import hive, {client} from 'utils/dhive';
 import Toast from 'react-native-simple-toast';
@@ -24,6 +31,7 @@ const Convert = ({
 }) => {
   const [amount, setAmount] = useState('');
   const [loading, setLoading] = useState(false);
+  const [showConversionsList, setShowConversionsList] = useState(false);
 
   useEffect(() => {
     fetchConversionRequestsConnect(user.name);
@@ -77,6 +85,43 @@ const Convert = ({
         value={amount}
         onChangeText={setAmount}
       />
+      <Separator />
+      <TouchableOpacity
+        onPress={() => {
+          setShowConversionsList(!showConversionsList);
+        }}>
+        <Text
+          style={
+            styles.conversions
+          }>{`${conversions.length} conversions`}</Text>
+      </TouchableOpacity>
+      <Separator />
+      {showConversionsList ? (
+        <FlatList
+          data={conversions}
+          style={styles.conversionContainer}
+          renderItem={({item}) => {
+            const [amt, currency] = item.amount.split(' ');
+            return (
+              <View style={styles.conversionRow}>
+                <Text>
+                  {amt} <Text style={styles.green}>{currency}</Text>
+                </Text>
+                <Text>-</Text>
+                <Text>
+                  {item.conversion_date
+                    .replace('T', ' ')
+                    .replace('-', '/')
+                    .replace('-', '/')}
+                </Text>
+              </View>
+            );
+          }}
+          keyExtractor={(conversion) => conversion.id}
+        />
+      ) : (
+        <View style={styles.conversionContainer} />
+      )}
 
       <Separator height={40} />
       <ActiveOperationButton
@@ -93,6 +138,19 @@ const getDimensionedStyles = (color) =>
   StyleSheet.create({
     button: {backgroundColor: '#68A0B4'},
     currency: {fontWeight: 'bold', fontSize: 18, color},
+    conversions: {
+      fontWeight: 'bold',
+    },
+    conversionRow: {
+      width: '70%',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    conversionContainer: {
+      height: 80,
+    },
+    green: {color: '#005C09'},
   });
 
 export default connect(
