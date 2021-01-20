6707743fa135b801ea3fc9f325f7634c8b793213
stoodkev <quentin.correa@gmail.com>
2021-01-20 16:01:44 +0100

    add custom picker to new file

---
+++ src/components/form/CustomPicker.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import {
+  StyleSheet,
+  useWindowDimensions,
+  Platform,
+  TouchableOpacity,
+  ActionSheetIOS,
+  Text,
+} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
+import {translate} from 'utils/localize';
+
+const CustomPicker = ({list, selectedValue, onSelected}) => {
+  const {width, height} = useWindowDimensions();
+  const styles = getDimensionedStyles({width, height});
+  switch (Platform.OS) {
+    case 'ios':
+      return (
+        <TouchableOpacity
+          style={styles.iosContainer}
+          onPress={() => {
+            ActionSheetIOS.showActionSheetWithOptions(
+              {
+                options: list,
+              },
+              (index) => {
+                onSelected(list[index]);
+              },
+            );
+          }}>
+          <Text style={styles.iosLabel}>@{selectedValue}</Text>
+          <Text>{'\u25BC'}</Text>
+        </TouchableOpacity>
+      );
+    case 'android':
+      console.log(list, selectedValue, onSelected);
+
+      return (
+        <Picker
+          style={styles.picker}
+          selectedValue={'selectedValue'}
+          prompt={translate('components.picker.prompt')}
+          onValueChange={onSelected}>
+          {list.map((item) => {
+            console.log(item);
+            return <Picker.Item key={item} label={`@${item}`} value={item} />;
+          })}
+        </Picker>
+      );
+    default:
+      break;
+  }
+};
+
+const getDimensionedStyles = ({width, height}) =>
+  StyleSheet.create({
+    iosContainer: {
+      flex: 1,
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      flexDirection: 'row',
+      marginRight: 20,
+    },
+    iosLabel: {fontSize: 18},
+    picker: {width: '100%', color: 'black'},
+  });
+
+export default CustomPicker;
