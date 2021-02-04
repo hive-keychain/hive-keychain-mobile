8b1df95294950e1789d27c2aef821a4a84be8e09
stoodkev <quentin.correa@gmail.com>
2021-02-04 15:54:07 +0800

    remove unneeded dependencies

--- src/components/ui/FocusAwareStatusBar.js
+++ src/components/ui/FocusAwareStatusBar.js
@@ -1,28 +1,11 @@
 import * as React from 'react';
-import {StatusBar, Platform, View} from 'react-native';
+import {StatusBar} from 'react-native';
 import {useIsFocused} from '@react-navigation/native';
-import {getStatusBarHeight} from 'react-native-status-bar-height';
 
 export default (props) => {
-  const STATUS_BAR_HEIGHT = Platform.OS === 'ios' ? getStatusBarHeight() : 0;
-
   if (!useIsFocused()) {
     return null;
+  } else {
+    return <StatusBar {...props} />;
   }
-  return <StatusBar {...props} />;
-  // switch (Platform.OS) {
-  //   case 'android':
-  //     return <StatusBar {...props} />;
-  //   case 'ios':
-  //     return (
-  //       <View
-  //         style={{
-  //           width: '100%',
-  //           height: STATUS_BAR_HEIGHT,
-  //           backgroundColor: props.backgroundColor,
-  //         }}>
-  //         <StatusBar barStyle="light-content" />
-  //       </View>
-  //     );
-  // }
 };
