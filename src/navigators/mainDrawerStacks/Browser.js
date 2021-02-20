3580f4cafd9472b2d3fe87534b7a87ca3bc51acd
stoodkev <quentin.correa@gmail.com>
2021-02-20 15:41:39 +0800

    add Browser screen

---
+++ src/navigators/mainDrawerStacks/Browser.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import Browser from 'screens/Browser';
+import DrawerButton from 'components/ui/DrawerButton';
+import {headerTransparent} from 'utils/navigation';
+import {translate} from 'utils/localize';
+
+const Stack = createStackNavigator();
+
+export default () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="BrowserScreen"
+        component={Browser}
+        options={({navigation}) => ({
+          headerStyle: {
+            backgroundColor: 'black',
+          },
+          headerTitleAlign: 'left',
+          title: translate('navigation.browser'),
+          headerTintColor: 'white',
+          headerRight: () => <DrawerButton navigation={navigation} />,
+          headerTransparent,
+        })}
+      />
+    </Stack.Navigator>
+  );
+};
---
+++ src/screens/Browser.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import Background from 'components/ui/Background';
+
+const Browser = () => {
+  return (
+    <Background>
+      <Text>Browser</Text>
+    </Background>
+  );
+};
+
+export default Browser;
