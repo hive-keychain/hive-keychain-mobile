248016c1645287373ebc227dfcd3caff238d88d7
stoodkev <quentin.correa@gmail.com>
2021-03-17 17:00:55 +0800

    request modal titles

--- src/components/browser/RequestModalContent.js
+++ src/components/browser/RequestModalContent.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
 import Operation from 'components/operations/Operation';
+import {translate} from 'utils/localize';
 
 export default ({accounts, request, onForceCloseModal}) => {
   const renderOperationDetails = () => {
     return null;
   };
+  const getOperationTitle = ({type, title}) => {
+    if (type === 'signBuffer' && title) {
+      return title;
+    }
+    console.log(`request.title.${type}`, translate(`request.title.${type}`));
+    return translate(`request.title.${type}`);
+  };
+  //TODO : add dApp icon
   return (
-    <Operation title="a" onClose={onForceCloseModal}>
+    <Operation title={getOperationTitle(request)} onClose={onForceCloseModal}>
       {renderOperationDetails()}
     </Operation>
   );
--- src/screens/Modal.js
+++ src/screens/Modal.js
@@ -5,7 +5,6 @@ export default ({navigation, route}) => {
   const onForceCloseModal = route.params
     ? route.params.onForceCloseModal
     : null;
-  console.log(onForceCloseModal, route.params);
   return (
     <CustomModal
       outsideClick={
