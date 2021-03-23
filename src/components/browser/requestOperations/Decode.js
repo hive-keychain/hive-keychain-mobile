346efa353eb2a22f41d6d3ffb6bd55990deffd49
stoodkev <quentin.correa@gmail.com>
2021-03-23 14:04:30 +0800

    extract requestOperation logic in a HOC

--- src/components/browser/requestOperations/Decode.js
+++ src/components/browser/requestOperations/Decode.js
@@ -1,12 +1,9 @@
-import React, {useState} from 'react';
-import {View, StyleSheet} from 'react-native';
+import React from 'react';
 import RequestItem from './components/RequestItem';
-import RequestMessage from './components/RequestMessage';
-import RequestResultMessage from './components/RequestResultMessage';
-import OperationButton from 'components/form/ActiveOperationButton';
 import {translate} from 'utils/localize';
 import {urlTransformer} from 'utils/browser';
 import {decodeMemo} from 'components/bridge';
+import RequestOperation from './RequestOperation';
 
 export default ({
   request,
@@ -17,69 +14,35 @@ export default ({
 }) => {
   const {request_id, ...data} = request;
   const {domain, method, username, message} = data;
-  const [loading, setLoading] = useState(false);
-  const [resultMessage, setResultMessage] = useState(null);
-  const renderRequestSummary = () => (
-    <View>
-      <RequestMessage
-        message={translate('request.message.decode', {
-          domain: urlTransformer(domain).hostname,
-          method,
-          username,
-        })}
-      />
+
+  return (
+    <RequestOperation
+      message={translate('request.message.decode', {
+        domain: urlTransformer(domain).hostname,
+        method,
+        username,
+      })}
+      sendResponse={sendResponse}
+      sendError={sendError}
+      successMessage={translate('request.success.decode')}
+      errorMessage={translate('request.error.decode')}
+      method={method}
+      request={request}
+      closeGracefully={closeGracefully}
+      performOperation={async () => {
+        const account = accounts.find((e) => e.name === request.username);
+        const key = account.keys[method.toLowerCase()];
+        const result = await decodeMemo(key, message);
+        if (result === message) {
+          throw 'error';
+        }
+        return result;
+      }}>
       <RequestItem
         title={translate('request.item.username')}
         content={`@${username}`}
       />
       <RequestItem title={translate('request.item.method')} content={method} />
-      <OperationButton
-        style={styles.button}
-        title={translate('request.confirm')}
-        isLoading={loading}
-        method={method.toLowerCase()}
-        onPress={async () => {
-          setLoading(true);
-          const account = accounts.find((e) => e.name === request.username);
-          const key = account.keys[method.toLowerCase()];
-          let msg;
-          try {
-            const result = await decodeMemo(key, message);
-            if (result === message) {
-              throw 'error';
-            }
-            msg = translate('request.success.decode');
-
-            sendResponse({
-              data,
-              request_id,
-              result,
-              message: msg,
-            });
-          } catch (e) {
-            msg = translate('request.error.decode');
-            sendError({data, request_id, error: {}, message: msg});
-          } finally {
-            setResultMessage(msg);
-          }
-          setLoading(false);
-        }}
-      />
-    </View>
+    </RequestOperation>
   );
-
-  if (resultMessage) {
-    return (
-      <RequestResultMessage
-        closeGracefully={closeGracefully}
-        resultMessage={resultMessage}
-      />
-    );
-  } else {
-    return renderRequestSummary();
-  }
 };
-
-const styles = StyleSheet.create({
-  button: {marginTop: 40},
-});
---
+++ src/components/browser/requestOperations/RequestOperation.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import {View, StyleSheet} from 'react-native';
+import RequestMessage from './components/RequestMessage';
+import RequestResultMessage from './components/RequestResultMessage';
+import OperationButton from 'components/form/ActiveOperationButton';
+import {translate} from 'utils/localize';
+
+export default ({
+  closeGracefully,
+  sendResponse,
+  sendError,
+  message,
+  children,
+  method,
+  request,
+  successMessage,
+  errorMessage,
+  performOperation,
+}) => {
+  const {data, request_id} = request;
+  const [loading, setLoading] = useState(false);
+  const [resultMessage, setResultMessage] = useState(null);
+
+  const renderRequestSummary = () => (
+    <View>
+      <RequestMessage message={message} />
+      {children}
+      <OperationButton
+        style={styles.button}
+        title={translate('request.confirm')}
+        isLoading={loading}
+        method={method.toLowerCase()}
+        onPress={async () => {
+          setLoading(true);
+          let msg;
+          try {
+            const result = await performOperation();
+            msg = successMessage;
+
+            sendResponse({
+              data,
+              request_id,
+              result,
+              message: msg,
+            });
+          } catch (e) {
+            msg = errorMessage;
+            sendError({data, request_id, error: {}, message: msg});
+          } finally {
+            setResultMessage(msg);
+          }
+          setLoading(false);
+        }}
+      />
+    </View>
+  );
+
+  if (resultMessage) {
+    return (
+      <RequestResultMessage
+        closeGracefully={closeGracefully}
+        resultMessage={resultMessage}
+      />
+    );
+  } else {
+    return renderRequestSummary();
+  }
+};
+
+const styles = StyleSheet.create({
+  button: {marginTop: 40},
+});
