480e370da7582cb5be2b18e234a40a41227923bd
stoodkev <quentin.correa@gmail.com>
2021-04-06 10:36:39 +0800

    Transfer operation

---
+++ src/components/browser/requestOperations/Transfer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import RequestItem from './components/RequestItem';
+import {translate} from 'utils/localize';
+import {transfer} from 'utils/hive';
+import RequestOperation from './components/RequestOperation';
+import usePotentiallyAnonymousRequest from 'hooks/usePotentiallyAnonymousRequest';
+
+export default ({
+  request,
+  accounts,
+  closeGracefully,
+  sendResponse,
+  sendError,
+}) => {
+  const {request_id, ...data} = request;
+  const {to, memo, amount, currency} = data;
+  const {
+    getUsername,
+    getAccountKey,
+    RequestUsername,
+  } = usePotentiallyAnonymousRequest(request, accounts);
+
+  return (
+    <RequestOperation
+      sendResponse={sendResponse}
+      sendError={sendError}
+      successMessage={translate('request.success.transfer')}
+      errorMessage={translate('request.error.transfer')}
+      method={'active'}
+      request={request}
+      closeGracefully={closeGracefully}
+      performOperation={async () => {
+        return await transfer(getAccountKey(), {
+          from: getUsername(),
+          to,
+          memo,
+          amount: `${amount} ${currency}`,
+        });
+      }}>
+      <RequestUsername />
+      <RequestItem title={translate('request.item.to')} content={`@${to}`} />
+      <RequestItem
+        title={translate('request.item.amount')}
+        content={`${amount} ${currency}`}
+      />
+      <RequestItem title={translate('request.item.memo')} content={memo} />
+    </RequestOperation>
+  );
+};
--- src/components/browser/requestOperations/index.js
+++ src/components/browser/requestOperations/index.js
@@ -2,10 +2,12 @@ import Decode from './Decode';
 import SignBuffer from './SignBuffer';
 import Vote from './Vote';
 import Custom from './Custom';
+import Transfer from './Transfer';
 
 export default {
   Decode,
   SignBuffer,
   Vote,
   Custom,
+  Transfer,
 };
