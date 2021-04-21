6b1fd3e550c7c270bef66ef8e53789165ffff6fd
stoodkev <quentin.correa@gmail.com>
2021-04-21 11:25:52 +0800

    add fuzzy search autocomplete to modal url

--- src/components/browser/urlModal/UrlAutocomplete.js
+++ src/components/browser/urlModal/UrlAutocomplete.js
@@ -1,8 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet, TouchableOpacity, Text, Image} from 'react-native';
 import Fuse from 'fuse.js';
 
 export default ({onDismiss, input, onSubmit, history}) => {
+  const [candidates, setCandidates] = useState([]);
+
+  useEffect(() => {
+    const fuse = new Fuse(history, {
+      shouldSort: true,
+      threshold: 0.45,
+      location: 0,
+      distance: 100,
+      maxPatternLength: 32,
+      minMatchCharLength: 1,
+      keys: [
+        {name: 'name', weight: 0.5},
+        {name: 'url', weight: 0.5},
+      ],
+    });
+    const fuseSearchResult = fuse.search(input);
+    if (Array.isArray(fuseSearchResult)) {
+      setCandidates(fuseSearchResult.map((e) => e.item));
+    } else {
+      setCandidates([]);
+    }
+  }, [input, history]);
+
   const renderItem = ({name, url, icon}) => {
     return (
       <TouchableOpacity
@@ -23,7 +46,7 @@ export default ({onDismiss, input, onSubmit, history}) => {
       </TouchableOpacity>
     );
   };
-  return history.map((e) => renderItem(e));
+  return candidates.map((e) => renderItem(e));
 };
 
 const styles = StyleSheet.create({
