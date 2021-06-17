a5dbff384a264c256c03fecf121c6f156a2adfbe
stoodkev <quentin.correa@gmail.com>
2021-06-17 14:52:44 +0800

    type settings screens

---
+++ src/screens/settings/About.tsx
@@ -0,0 +1,88 @@
+import Background from 'components/ui/Background';
+import Separator from 'components/ui/Separator';
+import React from 'react';
+import {Linking, StatusBar, StyleSheet, Text, View} from 'react-native';
+import VersionInfo from 'react-native-version-info';
+
+export default () => {
+  return (
+    <Background>
+      <>
+        <StatusBar backgroundColor="black" />
+        <Separator height={50} />
+        <View style={styles.container}>
+          <Text style={styles.title}>
+            Keychain Mobile v{VersionInfo.appVersion} (Beta)
+          </Text>
+          <Text style={styles.text}>
+            Keychain for mobile is developed by{' '}
+            <Text
+              style={styles.link}
+              onPress={() => {
+                Linking.openURL('https://peakd.com/@stoodkev');
+              }}>
+              @stoodkev
+            </Text>{' '}
+            and designed by{' '}
+            <Text
+              style={styles.link}
+              onPress={() => {
+                Linking.openURL('https://peakd.com/@nateaguila');
+              }}>
+              @nateaguila
+            </Text>
+            .
+          </Text>
+          <Separator height={20} />
+          <Text style={styles.text}>
+            It originates from Hive Keychain, a browser extension imagined by
+            the Splinterlands' funders{' '}
+            <Text
+              style={styles.link}
+              onPress={() => {
+                Linking.openURL('https://peakd.com/@yabapmatt');
+              }}>
+              @yabapmatt
+            </Text>{' '}
+            and{' '}
+            <Text
+              style={styles.link}
+              onPress={() => {
+                Linking.openURL('https://peakd.com/@aggroed');
+              }}>
+              @aggroed
+            </Text>
+            .
+          </Text>
+          <Separator height={20} />
+          <Text style={styles.text}>
+            Since the application is still in Beta, we count on you to report
+            bugs, layout issues and improvement ideas on our{' '}
+            <Text
+              style={styles.link}
+              onPress={() => {
+                Linking.openURL(
+                  'https://github.com/stoodkev/hive-keychain-mobile',
+                );
+              }}>
+              Github.
+            </Text>
+          </Text>
+        </View>
+      </>
+    </Background>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {marginVertical: 60, marginHorizontal: 20},
+  link: {color: 'lightgrey'},
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 40,
+  },
+  text: {color: 'white', fontSize: 16, textAlign: 'justify'},
+});
---
+++ src/screens/settings/Settings.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {connect} from 'react-redux';
+import FocusAwareStatusBar from 'components/ui/FocusAwareStatusBar';
+import SafeArea from 'components/ui/SafeArea';
+import Separator from 'components/ui/Separator';
+import {setRpc} from 'actions';
+import CustomPicker from 'components/form/CustomPicker';
+import {rpcList} from 'utils/hiveUtils';
+import {translate} from 'utils/localize';
+
+const AccountManagement = ({navigation, setRpc, settings}) => {
+  return (
+    <SafeArea>
+      <FocusAwareStatusBar barStyle="light-content" backgroundColor="black" />
+      <View style={styles.view}>
+        <Text style={styles.title}>{translate('settings.settings.title')}</Text>
+        {translate('settings.settings.disclaimer').map((disclaimer, i) => (
+          <Text key={i} style={styles.disclaimer}>
+            {disclaimer}
+          </Text>
+        ))}
+        <Separator height={20} />
+        <CustomPicker
+          onSelected={setRpc}
+          selectedValue={settings.rpc}
+          list={rpcList}
+          prompt={translate('components.picker.prompt_rpc')}
+        />
+      </View>
+    </SafeArea>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {paddingHorizontal: 20, backgroundColor: 'white', height: '100%'},
+  title: {
+    color: '#404950',
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginVertical: 15,
+    textTransform: 'uppercase',
+  },
+  disclaimer: {color: '#404950', marginVertical: 2},
+  button: {backgroundColor: '#B9122F'},
+});
+
+const mapStateToProps = (state) => ({
+  settings: state.settings,
+});
+
+export default connect(mapStateToProps, {
+  setRpc,
+})(AccountManagement);
