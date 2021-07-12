7ee2ce32cbfb6b212b419423e4587a875b2232ed
stoodkev <quentin.correa@gmail.com>
2021-07-12 18:03:23 +0800

    add screens to ga

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import {NavigationContainer} from '@react-navigation/native';
+import analytics from '@react-native-firebase/analytics';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {addTabFromLinking} from 'actions/browser';
 import Bridge from 'components/bridge';
 import MainDrawer from 'navigators/MainDrawer';
 import SignUpStack from 'navigators/SignUp';
 import UnlockStack from 'navigators/Unlock';
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import {connect, ConnectedProps} from 'react-redux';
 import Modal from 'screens/Modal';
 import {RootState} from 'store';
@@ -17,6 +21,8 @@ import {RootStackParam} from './navigators/Root.types';
 const Root = createStackNavigator<RootStackParam>();
 
 const App = ({hasAccounts, auth, rpc, addTabFromLinking}: PropsFromRedux) => {
+  let routeNameRef: React.MutableRefObject<string> = useRef();
+  let navigationRef: React.MutableRefObject<NavigationContainerRef> = useRef();
   useEffect(() => {
     setupLinking(addTabFromLinking);
     return () => {
@@ -54,7 +60,29 @@ const App = ({hasAccounts, auth, rpc, addTabFromLinking}: PropsFromRedux) => {
   };
 
   return (
-    <NavigationContainer ref={(navigator) => setNavigator(navigator)}>
+    <NavigationContainer
+      ref={(navigator) => {
+        setNavigator(navigator);
+        navigationRef.current = navigator;
+      }}
+      onStateChange={async (state) => {
+        const previousRouteName = routeNameRef.current;
+        const currentRouteName = navigationRef.current.getCurrentRoute().name;
+
+        if (previousRouteName !== currentRouteName) {
+          // The line below uses the expo-firebase-analytics tracker
+          // https://docs.expo.io/versions/latest/sdk/firebase-analytics/
+          // Change this line to use another Mobile analytics SDK
+          console.log('routename', currentRouteName);
+          await analytics().logScreenView({
+            screen_name: currentRouteName,
+            screen_class: currentRouteName,
+          });
+        }
+
+        // Save the current route name for later comparison
+        routeNameRef.current = currentRouteName;
+      }}>
       {renderRootNavigator()}
       <Bridge />
     </NavigationContainer>
