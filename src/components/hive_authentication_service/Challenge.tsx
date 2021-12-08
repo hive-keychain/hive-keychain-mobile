ec5e6e8208fa4902579ac577a00e1594affa846f
stoodkev <quentin.correa@gmail.com>
2021-12-08 16:37:42 +0800

    add challenge has

---
+++ src/components/hive_authentication_service/Challenge.tsx
@@ -0,0 +1,121 @@
+import Hive from 'assets/wallet/icon_hive.svg';
+import EllipticButton from 'components/form/EllipticButton';
+import Operation from 'components/operations/Operation';
+import Separator from 'components/ui/Separator';
+import {ModalNavigation} from 'navigators/Root.types';
+import React, {useState} from 'react';
+import {StyleSheet, Text} from 'react-native';
+import {connect, ConnectedProps} from 'react-redux';
+import {RootState} from 'store';
+import {
+  Connection,
+  HAS_ChallengeReq,
+} from 'utils/hiveAuthenticationService.types';
+import {KeychainKeyTypesLC} from 'utils/keychain.types';
+import {translate} from 'utils/localize';
+
+type Props = PropsFromRedux & {
+  data: HAS_ChallengeReq & {
+    callback: (
+      payload: HAS_ChallengeReq,
+      approve: boolean,
+      connection: Connection,
+      callback: (success: boolean) => void,
+    ) => void;
+    domain: string;
+    connection: Connection;
+  };
+  navigation: ModalNavigation;
+};
+
+enum ChallengeState {
+  ASK,
+  SUCCESS,
+  ERROR,
+}
+const HASChallengeRequest = ({data, accounts, navigation}: Props) => {
+  const [state, setState] = useState(ChallengeState.ASK);
+  const onConfirm = () => {
+    data.callback(data, true, data.connection, (success: boolean) => {
+      setState(success ? ChallengeState.SUCCESS : ChallengeState.ERROR);
+      setTimeout(() => {
+        navigation.goBack();
+      }, 3000);
+    });
+  };
+  const showText = () => {
+    switch (state) {
+      case ChallengeState.ASK:
+        return translate('wallet.has.challenge.ask', {
+          account: data.account,
+          key: data.decrypted_data.key_type,
+          name: data.domain,
+        });
+      case ChallengeState.SUCCESS:
+        return translate('wallet.has.challenge.success', {
+          account: data.account,
+          key: data.decrypted_data.key_type,
+          name: data.domain,
+        });
+    }
+  };
+
+  return (
+    <Operation logo={<Hive />} title={translate('request.title.decode')}>
+      <>
+        <Separator height={30} />
+        <Text style={styles.uuid}>{translate('wallet.has.uuid', data)}</Text>
+        <Separator />
+        <Text>{showText()}</Text>
+        {accounts.find((e) => e.name === data.account) ? null : (
+          <>
+            <Separator />
+            <Text style={styles.error}>
+              {translate('wallet.has.challenge.no_key', {
+                account: data.account,
+                key: data.decrypted_data.key_type,
+                name: data.domain,
+              })}
+            </Text>
+          </>
+        )}
+        <Separator height={50} />
+        <EllipticButton
+          title={
+            state !== ChallengeState.ASK
+              ? translate('common.ok')
+              : translate('wallet.has.challenge.button')
+          }
+          disabled={
+            !accounts.find((e) => e.name === data.account).keys[
+              data.decrypted_data.key_type as KeychainKeyTypesLC
+            ]
+          }
+          onPress={
+            state !== ChallengeState.ASK
+              ? () => {
+                  navigation.goBack();
+                }
+              : onConfirm
+          }
+          style={styles.button}
+        />
+      </>
+    </Operation>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {backgroundColor: '#68A0B4'},
+  error: {color: '#A3112A'},
+  uuid: {fontWeight: 'bold'},
+});
+
+const connector = connect((state: RootState) => {
+  return {
+    accounts: state.accounts,
+  };
+});
+
+type PropsFromRedux = ConnectedProps<typeof connector>;
+export default connector(HASChallengeRequest);
