3bf7f366d6cbc9d9306534ed138410373738f606
theghost1980 <saturnob612@gmail.com>
2022-09-29 10:05:17 -0700

    tests(initial configuration completed)

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  preset: 'react-native',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  testPathIgnorePatterns: ['\\.snap$', '<rootDir>/node_modules/'],
+  transformIgnorePatterns: ['node_modules/?!(static-container)'],
+  //cacheDirectory: '.jest/cache',
+  // globals: {
+  //   'ts-jest': {
+  //     isolatedModules: true,
+  //   },
+  // },
+  clearMocks: false,
+  modulePathIgnorePatterns: ['utils-for-testing', 'mocks', 'othercases'],
+  setupFilesAfterEnv: [
+    './jest.setup.js',
+    //'./node_modules/react-native-gesture-handler/jestSetup.js',
+  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/node_modules/**'],
+};
