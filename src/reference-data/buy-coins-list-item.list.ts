2a9ee95f8335a8edae8337da2f9f3ef14fecbd35
stoodkev <quentin.correa@gmail.com>
2023-05-17 12:00:57 +0800

    removed bt for now

--- src/reference-data/buy-coins-list-item.list.ts
+++ src/reference-data/buy-coins-list-item.list.ts
@@ -12,12 +12,12 @@ export const BuyCoinsListItem = (type: BuyCoinType, username: string) => {
             link: `https://global.transak.com?apiKey=${TransakConfig.apiKey}&defaultCryptoCurrency=HIVE&exchangeScreenTitle=Buy%20HIVEs&isFeeCalculationHidden=true&walletAddress=${username}`,
             description: 'html_popup_transak_description',
           },
-          {
-            name: 'Blocktrades',
-            image: 'blocktrades.svg',
-            link: `https://blocktrades.us/en/trade?affiliate_id=dfccdbcb-6093-4e4a-992d-689bf46e2523&input_coin_type=btc&output_coin_type=hive&output_coin_amount=10&receive_address=${username}`,
-            description: 'html_popup_blocktrades_description',
-          },
+          // {
+          //   name: 'Blocktrades',
+          //   image: 'blocktrades.svg',
+          //   link: `https://blocktrades.us/en/trade?affiliate_id=dfccdbcb-6093-4e4a-992d-689bf46e2523&input_coin_type=btc&output_coin_type=hive&output_coin_amount=10&receive_address=${username}`,
+          //   description: 'html_popup_blocktrades_description',
+          // },
         ],
         exchanges: [
           {
@@ -66,12 +66,12 @@ export const BuyCoinsListItem = (type: BuyCoinType, username: string) => {
     case BuyCoinType.BUY_HDB:
       return {
         list: [
-          {
-            name: 'Blocktrades',
-            image: 'blocktrades.svg',
-            link: `https://blocktrades.us/en/trade?affiliate_id=dfccdbcb-6093-4e4a-992d-689bf46e2523&input_coin_type=btc&output_coin_type=hbd&output_coin_amount=10&receive_address=${username}`,
-            description: 'html_popup_blocktrades_description',
-          },
+          // {
+          //   name: 'Blocktrades',
+          //   image: 'blocktrades.svg',
+          //   link: `https://blocktrades.us/en/trade?affiliate_id=dfccdbcb-6093-4e4a-992d-689bf46e2523&input_coin_type=btc&output_coin_type=hbd&output_coin_amount=10&receive_address=${username}`,
+          //   description: 'html_popup_blocktrades_description',
+          // },
         ],
         exchanges: [
           {
