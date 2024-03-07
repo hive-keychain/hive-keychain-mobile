1b44e6b7601604865ddb020b96545dbf887648f7
theghost1980 <saturnob612@gmail.com>
2024-03-07 10:26:16 -0400

    fix: Menu : all menus should look like cards

--- src/screens/hive/settings/SettingsMenu.tsx
+++ src/screens/hive/settings/SettingsMenu.tsx
@@ -3,10 +3,11 @@ import Icon from 'components/hive/Icon';
 import {useBackButtonNavigation} from 'hooks/useBackButtonNavigate';
 import {TemplateStackProps} from 'navigators/Root.types';
 import React from 'react';
-import {StatusBar, StyleSheet, View} from 'react-native';
-import {Theme, useThemeContext} from 'src/context/theme.context';
+import {StatusBar, View} from 'react-native';
+import {useThemeContext} from 'src/context/theme.context';
 import {Icons} from 'src/enums/icons.enums';
 import {PRIMARY_RED_COLOR, getColors} from 'src/styles/colors';
+import {getMenuCardStyle} from 'src/styles/menu';
 import {translate} from 'utils/localize';
 import {navigate} from 'utils/navigation';
 import Operations from './Operations';
@@ -14,12 +15,11 @@ import RpcNodes from './RpcNodes';
 
 const SettingsMenu = () => {
   const {theme} = useThemeContext();
-  const styles = getStyles(theme);
 
   useBackButtonNavigation('WALLET');
 
   return (
-    <View style={styles.container}>
+    <View style={getMenuCardStyle(theme)}>
       <StatusBar
         barStyle={getColors(theme).barStyle}
         backgroundColor={getColors(theme).primaryBackground}
@@ -57,18 +57,3 @@ const SettingsMenu = () => {
 };
 
 export default SettingsMenu;
-
-const getStyles = (theme: Theme) =>
-  StyleSheet.create({
-    container: {
-      borderRadius: 11,
-      borderWidth: 1,
-      borderColor: getColors(theme).cardBorderColorContrast,
-      backgroundColor: getColors(theme).cardBgLighter,
-      paddingVertical: 25,
-      paddingHorizontal: 14,
-    },
-    text: {
-      color: getColors(theme).secondaryText,
-    },
-  });
---
+++ src/styles/menu.ts
@@ -0,0 +1,21 @@
+import {TextStyle, ViewStyle} from 'react-native';
+import {Theme} from 'src/context/theme.context';
+import {getColors} from './colors';
+
+export const getMenuCardStyle = (theme: Theme) => {
+  return {
+    borderRadius: 11,
+    borderWidth: 1,
+    borderColor: getColors(theme).cardBorderColorContrast,
+    backgroundColor: getColors(theme).cardBgLighter,
+    paddingVertical: 25,
+    paddingHorizontal: 14,
+    marginHorizontal: 16,
+  } as ViewStyle;
+};
+
+export const getMenuFontStyle = (theme: Theme) => {
+  return {
+    color: getColors(theme).secondaryText,
+  } as TextStyle;
+};
