81be64240257b6955cb6df876931f6821010b7f2
stoodkev <quentin.correa@gmail.com>
2025-09-10 11:02:35 +0800

    refactor(expo): Update metro config with watchfolders

--- metro.config.js
+++ metro.config.js
@@ -1,24 +1,32 @@
-const { getDefaultConfig } = require("@expo/metro-config");
+const {getDefaultConfig} = require('@expo/metro-config');
 const {
   wrapWithReanimatedMetroConfig,
 } = require('react-native-reanimated/metro-config');
+const path = require('path');
+const dotenv = require('dotenv');
+dotenv.config();
+const watchFolders = process.env.METRO_WATCH_FOLDERS
+  ? process.env.METRO_WATCH_FOLDERS.split(',').map((folder) =>
+      path.resolve(__dirname, folder.trim()),
+    )
+  : [];
 
 module.exports = (() => {
   const config = getDefaultConfig(__dirname);
 
-  const { transformer, resolver } = config;
+  const {transformer, resolver} = config;
 
   config.transformer = {
     ...transformer,
-    babelTransformerPath: require.resolve("react-native-svg-transformer/expo"),
-    _expoRelativeProjectRoot: __dirname
+    babelTransformerPath: require.resolve('react-native-svg-transformer/expo'),
+    _expoRelativeProjectRoot: __dirname,
   };
   config.resolver = {
     ...resolver,
-    assetExts: resolver.assetExts.filter((ext) => ext !== "svg"),
-    sourceExts: [...resolver.sourceExts, "svg"],
-   
+    assetExts: resolver.assetExts.filter((ext) => ext !== 'svg'),
+    sourceExts: [...resolver.sourceExts, 'svg'],
   };
+  config.watchFolders = watchFolders;
 
   return wrapWithReanimatedMetroConfig(config);
 })();
